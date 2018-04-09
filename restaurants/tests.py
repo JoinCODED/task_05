61447e78d715a3faec21f8fad17a3e6f55ebe963
DarthHamza <hamzamakia@gmail.com>
2018-04-09 17:25:43 +0300

    preped test file for taskl

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -12,6 +12,11 @@ class RestarantModelTestCase(TestCase):
             )
 
 class RestaurantViewTestCase(TestCase):
+    def setUp(self):
+        self.restaurant_1 = Restaurant.objects.create(name="Restaurant 1", description="This is Restaurant 1", opening_time="00:01:00", closing_time="23:59:00")
+        self.restaurant_2 = Restaurant.objects.create(name="Restaurant 2", description="This is Restaurant 2", opening_time="00:01:00", closing_time="23:59:00")
+        self.restaurant_3 = Restaurant.objects.create(name="Restaurant 3", description="This is Restaurant 3", opening_time="00:01:00", closing_time="23:59:00")
+
     def test_welcome_view(self):
         url = reverse("hello-world")
         response = self.client.get(url)
@@ -22,13 +27,22 @@ class RestaurantViewTestCase(TestCase):
     def test_list_view(self):
         list_url = reverse("restaurant-list")
         response = self.client.get(list_url)
-        self.assertTrue(isinstance(response.context['my_list'],list))
+        for restaurant in Restaurant.objects.all():
+            self.assertIn(business, response.context['restaurants'])
+            self.assertContains(response, restaurant.name)
+            self.assertContains(response, restaurant.description)
+            self.assertContains(response, restaurant.opening_time)
+            self.assertContains(response, restaurant.closing_time)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(response.status_code, 200)
 
     def test_detail_view(self):
-        detail_url = reverse("restaurant-detail")
+        detail_url = reverse("restaurant-detail", kwargs={"restaurant_id":self.restaurant_1.id})
         response = self.client.get(detail_url)
-        self.assertTrue(isinstance(response.context['my_object'],dict))
+        self.assertEqual(self.restaurant_1, response.context['restaurant'])
+        self.assertContains(response, self.restaurant_1.name)
+        self.assertContains(response, self.restaurant_1.description)
+        self.assertContains(response, restaurant_1.opening_time)
+        self.assertContains(response, restaurant_1.closing_time)
         self.assertTemplateUsed(response, 'detail.html')
         self.assertEqual(response.status_code, 200)
